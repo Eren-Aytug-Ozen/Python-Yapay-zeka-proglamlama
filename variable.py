c2187efe19aa78d0e04dcb2c3dff09e593955a45
Aytug Özen <aytuozen55@gmail.com>
2023-02-05 14:37:28 +0300

    ders 1 variable

---
+++ variable.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+#variable //değişken
+#variable
+#function
+#object
+#%%
+var1 = 10#integer
+var2 =15
+
+gun = "pazartesi" #string
+
+var3 = 10.0 #double
+
+#%%
+s = "bugun gunlerden pazartesi"
+variable=type = type (s)
+
+var1 = "ankara"
+var2 = "ist"
+var3 = var1+var2
+
+var4 ="100"
+var5 ="200"
+var6 =var4+var5
+uzunluk = len(var6)
